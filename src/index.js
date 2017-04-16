e04b16dc5c9d74080baa5ffde2fee5d5e8518747
Ivan Junas <ivanjunas22@gmail.com>
2017-04-16 17:16:23 +0200

    05-reducer with tests

---
+++ src/index.js
@@ -0,0 +1,46 @@
+
+import expect from 'expect';
+
+
+function counter(state, action) {
+	if (typeof state === undefined) {
+		return 0; // if state is undefined, return initial state 
+	}
+
+	if (action.type === 'INCREMENT') {
+		return state + 1
+	} else if (action.type === 'DECREMENT') {
+		return state - 1;
+	} else {
+		return state;
+	}
+
+}
+
+
+expect(
+	counter(0, { type: 'INCREMENT'})
+).toEqual(1);
+
+expect(
+	counter(1, { type: 'INCREMENT'})
+).toEqual(2);
+
+expect(
+	counter(2, { type: 'DECREMENT'})
+).toEqual(1);
+
+expect(
+	counter(1, { type: 'DECREMENT'})
+).toEqual(0);
+
+expect (
+	counter(22, { type: 'Something'})
+).toEqual(22);
+
+expect(
+	counter(null, { type: 'INCREMENT'})
+).toEqual(1);
+
+
+console.log('Tests finished ');
