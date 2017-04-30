9e4701bc4e038ecf3ca60f70e478396747891875
Ivan Junas <ivanjunas22@gmail.com>
2017-04-30 09:43:44 +0200

    34 - configure state

---
+++ src/configureStore.js
@@ -0,0 +1,26 @@
+import { createStore } from 'redux';
+import todoApp from './reducers';
+import { loadState, saveState} from './localStorage';
+import  throttle from 'lodash/throttle';
+
+
+const configureStore = () => {
+
+	const persistedState = loadState();
+
+	const store = createStore(todoApp, persistedState);
+
+	// protect serialization just one time per second
+	store.subscribe(throttle(
+		() => {
+			saveState({
+				todos: store.getState().todos
+			});
+		}, 
+		1000)
+	);
+
+	return store;
+};
+ 
+export default configureStore;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import todoApp from './reducers';
-import App from './components/App';
-import { loadState, saveState} from './localStorage';
-import  throttle from 'lodash/throttle';
+import configureStore from './configureStore';
+import Root from './components/Root';
 
-const persistedState = loadState();
-
-const store = createStore(todoApp, persistedState);
-
-// protect serialization just one time per second
-store.subscribe(throttle(
-	() => {
-		saveState({
-			todos: store.getState().todos
-		});
-	}, 
-	1000)
-);
+const store = configureStore();
 
 ReactDOM.render(
-	<Provider store={store}> 
-		<App />
-	</Provider>, 
+	<Root store={store} />, 
 	document.getElementById('root')
 );
